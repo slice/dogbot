f4a65a06396966bb6e9c1adb952b7b0467a813ac
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-04-07 18:32:55 -0700

    new cmd: add d?dogfact

--- dog/ext/fun.py
+++ dog/ext/fun.py
@@ -2,7 +2,13 @@ import aiohttp
 from discord.ext import commands
 from dog import Cog
 
-SHIBE_ONLINE = 'http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true'
+SHIBE_ENDPOINT = 'http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true'
+DOGFACTS_ENDPOINT = 'https://dog-api.kinduff.com/api/facts'
+
+async def _get_json(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.json()
 
 class Fun(Cog):
     @commands.command()
@@ -12,11 +18,16 @@ class Fun(Cog):
 
         Grabs a random Shiba Inu picture from shibe.online.
         """
+        await ctx.send((await _get_json(SHIBE_ENDPOINT))[0])
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(SHIBE_ONLINE) as shibe:
-                url = (await shibe.json())[0]
-                await ctx.send(url)
+    @commands.command()
+    async def dogfact(self, ctx):
+        """ Returns a random dog fact. """
+        facts = await _get_json(DOGFACTS_ENDPOINT)
+        if not facts['success']:
+            await ctx.send('I couldn\'t contact the Dog Facts API.')
+            return
+        await ctx.send(facts['facts'][0])
 
 def setup(bot):
     bot.add_cog(Fun(bot))
