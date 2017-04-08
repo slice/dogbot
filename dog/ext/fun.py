eaafca8f038c79d6dff3d6451205a8a425478d29
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-04-07 22:33:48 -0700

    new cmd: add d?wacky

--- dog/ext/fun.py
+++ dog/ext/fun.py
@@ -1,14 +1,30 @@
+import logging
 import aiohttp
+import discord
+from PIL import Image, ImageEnhance
 from discord.ext import commands
 from dog import Cog
+from io import BytesIO
 
 SHIBE_ENDPOINT = 'http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true'
 DOGFACTS_ENDPOINT = 'https://dog-api.kinduff.com/api/facts'
 
-async def _get_json(url):
+logger = logging.getLogger(__name__)
+
+async def _get(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
-            return await response.json()
+            return response
+
+async def _get_bytesio(url):
+    # can't use _get for some reason
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            return BytesIO(await resp.read())
+
+async def _get_json(url):
+    resp = await _get(url)
+    return await resp.json()
 
 class Fun(Cog):
     @commands.command()
@@ -19,6 +35,30 @@ class Fun(Cog):
         Grabs a random Shiba Inu picture from shibe.online.
         """
         await ctx.send((await _get_json(SHIBE_ENDPOINT))[0])
+
+    @commands.command()
+    async def wacky(self, ctx, who: discord.Member = None):
+        """ Turns your avatar into... """
+        if not who:
+            who = ctx.message.author
+        logger.info('wacky: get: %s', who.avatar_url)
+
+        async with ctx.channel.typing():
+            avatar_data = await _get_bytesio(who.avatar_url)
+
+            logger.info('wacky: enhancing...')
+            im = Image.open(avatar_data)
+            converter = ImageEnhance.Color(im)
+            im = converter.enhance(50)
+
+            _path = '/tmp/image.jpg'
+            logger.info('wacky: saving...')
+            im.save(_path, format='jpeg', quality=0)
+            logger.info('wacky: sending...')
+            await ctx.send(file=open(_path, 'rb'), filename='x.jpg')
+
+            # close images
+            avatar_data.close()
 
     @commands.command()
     async def dogfact(self, ctx):
