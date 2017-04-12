e9ed3e2e2021d5286f4d3ada5ebae4cd6db4e8aa
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-04-11 20:33:06 -0700

    :lipstick:

--- dog.py
+++ dog.py
@@ -24,7 +24,7 @@ except ModuleNotFoundError:
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.INFO)
 
-# pretty formatting
+# formatters
 console_fmt = logging.Formatter('[%(levelname)s] %(name)s: %(message)s')
 nice_fmt = logging.Formatter('%(asctime)s '
                              '[%(name)s %(levelname)s] %(message)s')
@@ -32,10 +32,13 @@ nice_fmt = logging.Formatter('%(asctime)s '
 # discord log should be DEBUG, but only in dog.log
 discord_logger = logging.getLogger('discord')
 discord_logger.setLevel(logging.DEBUG)
+
+# debug file handler, includes debug
 file_handler = logging.FileHandler(filename='dog_debug.log', encoding='utf-8',
                                    mode='w')
 file_handler.setFormatter(nice_fmt)
 
+# main file handler, only info
 file_sane_handler = logging.FileHandler(filename='dog.log', encoding='utf-8',
                                         mode='w')
 file_sane_handler.setFormatter(nice_fmt)
