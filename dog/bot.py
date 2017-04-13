686f7fb14fd767aa9d740a1058edcdda23c29e8d
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-04-12 22:46:13 -0700

    monitoring :eyes:

--- dog/bot.py
+++ dog/bot.py
@@ -4,6 +4,7 @@ import logging
 import discord
 import traceback
 from discord.ext import commands
+from dog.util import pretty_timedelta
 import dog_config as cfg
 
 logger = logging.getLogger(__name__)
@@ -29,6 +30,29 @@ class DogBot(commands.AutoShardedBot):
         short_prefix = min(self.command_prefix, key=len)
         help_game = discord.Game(name=f'{short_prefix}help')
         await self.change_presence(game=help_game)
+
+    async def monitor_send(self, *args, **kwargs):
+        monitor_channels = getattr(cfg, 'owner_monitor_channels', [])
+        channels = [self.get_channel(c) for c in monitor_channels]
+
+        # no monitor channels
+        if not channels:
+            return
+
+        for channel in channels:
+            await channel.send(*args, **kwargs)
+
+    async def on_guild_join(self, g):
+        diff = pretty_timedelta(datetime.datetime.utcnow() - g.created_at)
+        fmt = (f'\N{SMIRKING FACE} Added to new guild "{g.name}" (`{g.id}`)'
+               f', {len(g.members)} members, owned by {g.owner.mention}'
+               f' (`{g.owner.id}`). This guild was created {diff} ago.')
+        await self.monitor_send(fmt)
+
+    async def on_guild_remove(self, g):
+        fmt = (f'\N{LOUDLY CRYING FACE} Removed from guild "{g.name}"'
+               f' (`{g.id}`)!')
+        await self.monitor_send(fmt)
 
     async def config_is_set(self, guild, name):
         return await self.redis.exists(f'{guild.id}:{name}')
