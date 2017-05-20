2c9e7af083089d32f66b2d02801e9e8487faf800
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-05-19 20:22:44 -0700

    Remove debug logging file

--- dog.py
+++ dog.py
@@ -32,28 +32,20 @@ console_fmt = logging.Formatter('[%(levelname)s] %(name)s: %(message)s')
 nice_fmt = logging.Formatter('%(asctime)s '
                              '[%(name)s %(levelname)s] %(message)s', '%m/%d/%Y %I:%M:%S %p')
 
-# discord log should be DEBUG, but only in dog.log
-discord_logger = logging.getLogger('discord')
-discord_logger.setLevel(logging.DEBUG)
-
-# debug file handler, includes debug
-file_handler = logging.FileHandler(filename='dog_debug.log', encoding='utf-8')
-file_handler.setFormatter(nice_fmt)
+logging.getLogger('discord').setLevel(logging.INFO)
+logging.getLogger('dog').setLevel(logging.DEBUG)
 
 # main file handler, only info
-file_sane_handler = logging.FileHandler(filename='dog.log', encoding='utf-8')
-file_sane_handler.setFormatter(nice_fmt)
-file_sane_handler.setLevel(logging.INFO)
+file_handler = logging.FileHandler(filename='dog.log', encoding='utf-8')
+file_handler.setFormatter(nice_fmt)
 
-# stream handler (stdout), only handle INFO+
+# stream handler (stdout)
 stream = logging.StreamHandler()
 stream.setFormatter(console_fmt)
-stream.setLevel(logging.INFO)
 
 # handle from all logs
 root_logger.addHandler(stream)
 root_logger.addHandler(file_handler)
-root_logger.addHandler(file_sane_handler)
 
 logger = logging.getLogger('dog')
 
