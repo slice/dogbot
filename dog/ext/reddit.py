1d6bb528056216c8ba48189aa87d0b06d3d088f3
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-05-31 18:43:02 -0700

    Improve posting task

--- dog/ext/reddit.py
+++ dog/ext/reddit.py
@@ -19,61 +19,87 @@ logger = logging.getLogger(__name__)
 class Reddit(Cog):
     def __init__(self, bot):
         super().__init__(bot)
+
+        self.update_interval = 60 * 25 # 25 minutes
         self.feed_task = bot.loop.create_task(self.post_to_feeds())
 
+    async def update_feed(self, feed):
+        logger.debug('Feed update: cid=%d, sub=%s', feed['channel_id'], feed['subreddit'])
+
+        # get the channel
+        channel = self.bot.get_channel(feed['channel_id'])
+        if not channel:
+            # stale channel, continue
+            logger.debug('Stale channel, not updating. cid=%d', feed['channel_id'])
+            return
+
+        # type to signal an upcoming post
+        await channel.trigger_typing()
+
+        # appropriate post filter
+        def post_filter(post):
+            # if the channel is nsfw, no posts should be excluded. or else, only allow sfw posts
+            appropriate = True if channel.is_nsfw() else not post.over_18
+            return not post.stickied and appropriate
+
+        # get the sub
+        sub = self.bot.praw.subreddit(feed['subreddit'])
+
+        # check if it exists
+        try:
+            sub.fullname
+        except prawcore.exceptions.NotFound:
+            logger.debug('Sub not found, not updating. sub=%s', feed['subreddit'])
+            # TODO: remove the feed
+            return
+
+        # get some hot posts
+        posts = list(filter(post_filter, sub.hot(limit=75)))
+
+        if not posts:
+            logger.debug('Could not find a suitable post. sub=%s', feed['subreddit'])
+            return
+
+        post = random.choice(posts)
+
+        # detect images so we can directly put them in the embed
+        endings = ('.gif', '.jpeg', '.png', '.jpg', '.webp')
+        is_image = not post.is_self and any([post.url.endswith(ending) for ending in endings])
+
+        logger.debug('Feed make post: url=%s title=%s sub=%s', post.url, post.title, post.subreddit)
+
+        # post it!
+        embed = discord.Embed(title=post.title, url=post.url, description=post.selftext)
+        embed.set_author(name='/r/%s \N{EM DASH} /u/%s' % (post.subreddit, post.author))
+        if is_image:
+            embed.set_image(url=post.url)
+        try:
+            await channel.send(embed=embed)
+        except discord.Forbidden:
+            # not allowed to send to the channel?
+            logger.debug('Not allowed to post to feed channel in %d, cid=%d', feed['guild_id'], feed['channel_id'])
+
     async def post_to_feeds(self):
+        # guilds aren't available until the bot is ready, and this task begins before the bot
+        # is ready. so let's wait for it to be ready before updating feeds
         await self.bot.wait_until_ready()
+
         while True:
-            logger.debug('Posting to feeds!')
+            # sleep up here so we don't update immediately
+            # wait the main sleep interval
+            await asyncio.sleep(self.update_interval)
+
+            # fetch all feeds, and update them all
             async with self.bot.pgpool.acquire() as conn:
-                records = await conn.fetch('SELECT * FROM reddit_feeds')
-                for record in records:
-                    logger.debug('Feed post: cid=%d, sub=%s', record['channel_id'], record['subreddit'])
+                feeds = await conn.fetch('SELECT * FROM reddit_feeds')
 
-                    # get the channel
-                    channel = self.bot.get_channel(record['channel_id'])
-                    if not channel:
-                        # stale channel, continue
-                        logger.debug('Stale channel, not posting... cid=%d', record['channel_id'])
-                        continue
-                    await channel.trigger_typing()
+                # enumerate through all feeds
+                for feed in feeds:
+                    # wait a minute or two to prevent rate limiting (doesn't really help but w/e)
+                    await asyncio.sleep((random.random() + 1) * 60)
 
-                    # appropriate post filter
-                    def post_filter(post):
-                        appropriate = True if channel.is_nsfw() else not post.over_18
-                        return not post.stickied and appropriate
-
-                    # get the sub
-                    sub = self.bot.praw.subreddit(record['subreddit'])
-
-                    # check if it exists
-                    try:
-                        sub.fullname
-                    except prawcore.exceptions.NotFound:
-                        logger.debug('Sub not found. sub=%s', record['subreddit'])
-                        continue
-
-                    # get some hot posts
-                    posts = list(filter(post_filter, sub.hot(limit=75)))
-
-                    if not posts:
-                        logger.debug('Could not find a suitable post. sub=%s', record['subreddit'])
-                        continue
-
-                    post = random.choice(posts)
-
-                    endings = ('.gif', '.jpeg', '.png', '.jpg', '.webp')
-                    is_image = not post.is_self and any([post.url.endswith(ending) for ending in endings])
-
-                    logger.debug('Feed make post: url=%s title=%s sub=%s', post.url, post.title, post.subreddit)
-
-                    # post it!
-                    embed = discord.Embed(title=post.title, url=post.url, description=post.selftext)
-                    embed.set_author(name='/r/%s \N{EM DASH} /u/%s' % (post.subreddit, post.author))
-                    if is_image:
-                        embed.set_image(url=post.url)
-                    await channel.send(embed=embed)
-            await asyncio.sleep(60 * 10)
+                    # update the feed
+                    await self.update_feed(feed)
 
     @commands.group()
     @commands.guild_only()
@@ -96,11 +122,11 @@ class Reddit(Cog):
         Only Dogbot Moderators may run this command.
         """
         async with self.bot.pgpool.acquire() as conn:
-            records = await conn.fetch('SELECT * FROM reddit_feeds WHERE guild_id = $1', ctx.guild.id)
-            if not records:
+            feeds = await conn.fetch('SELECT * FROM reddit_feeds WHERE guild_id = $1', ctx.guild.id)
+            if not feeds:
                 return await ctx.send('No feeds found! Set one up with `d?reddit watch <channel> <subreddit>`. See `d?help '
                                       'reddit watch` for more information.')
-            text = '\n'.join(['\N{BULLET} <#{}> (/r/{})'.format(r['channel_id'], r['subreddit']) for r in records])
+            text = '\n'.join(['\N{BULLET} <#{}> (/r/{})'.format(r['channel_id'], r['subreddit']) for r in feeds])
             await ctx.send('**Feeds in {}:**\n\n{}'.format(ctx.guild.name, text))
 
     @reddit.command(aliases=['unwatch'])
