db5c219edc892d3f7f518a422b6d2032d26e2643
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-06-01 23:29:20 -0700

    Add Reddit hot command

--- dog/ext/reddit.py
+++ dog/ext/reddit.py
@@ -16,12 +16,53 @@ from dog.core import checks, utils
 logger = logging.getLogger(__name__)
 
 
+def create_post_embed(post) -> discord.Embed:
+    endings = ('.gif', '.jpeg', '.png', '.jpg', '.webp')
+    is_image = not post.is_self and any([post.url.endswith(ending) for ending in endings])
+
+    embed = discord.Embed(title=post.title, url=post.url, description=post.selftext)
+    embed.set_author(name='/r/%s \N{EM DASH} /u/%s' % (post.subreddit, post.author))
+    if is_image:
+        embed.set_image(url=post.url)
+    return embed
+
+
+
 class Reddit(Cog):
     def __init__(self, bot):
         super().__init__(bot)
 
         self.update_interval = 60 * 10 # 10 minutes
         self.feed_task = bot.loop.create_task(self.post_to_feeds())
+
+    async def get_hot(self, channel: discord.TextChannel, sub: str):
+        """ Returns a hot Post from a sub. """
+
+        # appropriate post filter
+        def post_filter(post):
+            # if the channel is nsfw, no posts should be excluded. or else, only allow sfw posts
+            appropriate = True if channel.is_nsfw() else not post.over_18
+            return not post.stickied and appropriate
+
+        # get the sub
+        sub = self.bot.praw.subreddit(sub)
+
+        # check if it exists
+        try:
+            sub.fullname
+        except prawcore.exceptions.NotFound:
+            logger.debug('Sub not found, not updating. sub=%s', sub)
+            return
+
+        # get some hot posts
+        posts = list(filter(post_filter, sub.hot(limit=75)))
+
+        if not posts:
+            logger.debug('Could not find a suitable post. sub=%s', sub)
+            return
+
+        # just grab a random post
+        return random.choice(posts)
 
     async def update_feed(self, feed):
         logger.debug('Feed update: cid=%d, sub=%s', feed['channel_id'], feed['subreddit'])
@@ -36,45 +77,14 @@ class Reddit(Cog):
         # type to signal an upcoming post
         await channel.trigger_typing()
 
-        # appropriate post filter
-        def post_filter(post):
-            # if the channel is nsfw, no posts should be excluded. or else, only allow sfw posts
-            appropriate = True if channel.is_nsfw() else not post.over_18
-            return not post.stickied and appropriate
-
-        # get the sub
-        sub = self.bot.praw.subreddit(feed['subreddit'])
-
-        # check if it exists
-        try:
-            sub.fullname
-        except prawcore.exceptions.NotFound:
-            logger.debug('Sub not found, not updating. sub=%s', feed['subreddit'])
-            # TODO: remove the feed
-            return
-
-        # get some hot posts
-        posts = list(filter(post_filter, sub.hot(limit=75)))
-
-        if not posts:
-            logger.debug('Could not find a suitable post. sub=%s', feed['subreddit'])
-            return
-
-        post = random.choice(posts)
-
-        # detect images so we can directly put them in the embed
-        endings = ('.gif', '.jpeg', '.png', '.jpg', '.webp')
-        is_image = not post.is_self and any([post.url.endswith(ending) for ending in endings])
+        # get a host post
+        post = await self.get_hot(channel, feed['subreddit'])
 
         logger.debug('Feed make post: url=%s title=%s sub=%s', post.url, post.title, post.subreddit)
 
         # post it!
-        embed = discord.Embed(title=post.title, url=post.url, description=post.selftext)
-        embed.set_author(name='/r/%s \N{EM DASH} /u/%s' % (post.subreddit, post.author))
-        if is_image:
-            embed.set_image(url=post.url)
         try:
-            await channel.send(embed=embed)
+            await channel.send(embed=create_post_embed(post))
         except discord.Forbidden:
             # not allowed to send to the channel?
             logger.debug('Not allowed to post to feed channel in %d, cid=%d', feed['guild_id'], feed['channel_id'])
@@ -100,6 +110,15 @@ class Reddit(Cog):
 
                     # update the feed
                     await self.update_feed(feed)
+
+    @commands.command()
+    async def hot(self, ctx, sub: str):
+        """ Fetches hot posts from a subreddit. """
+        try:
+            post = await self.get_hot(ctx.channel, sub)
+            await ctx.send(embed=create_post_embed(post))
+        except:
+            await ctx.send('Failed to grab a post.')
 
     @commands.group()
     @commands.guild_only()
