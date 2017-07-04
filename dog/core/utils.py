b337d2d1211c8574abf9f8fa4d7ecd13110fb81e
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-07-04 09:40:53 -0700

    Fix incorrect Enum detection for EnumConverter

--- dog/core/utils.py
+++ dog/core/utils.py
@@ -178,7 +178,7 @@ class EnumConverter:
     @classmethod
     async def convert(cls, ctx, arg):
         try:
-            if arg not in cls:
+            if arg not in [e.name for e in cls]:
                 return cls[arg.upper()]
             else:
                 return cls[arg]
