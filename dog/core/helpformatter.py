3500255cca4f5d119a8bb1e337e708113d32cbfa
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-07-16 16:17:58 -0700

    Disable custom help formatter

--- dog/core/helpformatter.py
+++ dog/core/helpformatter.py
@@ -4,6 +4,7 @@ INVITE = 'https://discord.gg/JMYdXtq'
 
 
 class DogbotHelpFormatter(commands.HelpFormatter):
-    def get_ending_note(self):
-        note = super().get_ending_note()
-        return note if not self.is_bot() else note + '\nNeed help? Visit the support server: ' + INVITE
+    pass
+    # def get_ending_note(self):
+    #     note = super().get_ending_note()
+    #     return note if not self.is_bot() else note + '\nNeed help? Visit the support server: ' + INVITE
