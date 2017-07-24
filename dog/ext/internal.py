537763259af8236f313c61864cfbc660d4b6ab39
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-07-24 01:04:48 -0700

    Remove a bunch of internal commands

--- dog/ext/internal.py
+++ dog/ext/internal.py
@@ -15,11 +15,6 @@ from discord.ext import commands
 from dog import Cog
 from dog.core import utils, converters
 
-DETAILED_PING = '''**message create:** {}
-**message edit:** {}
-**message delete:** {}
-'''
-
 logger = logging.getLogger(__name__)
 
 
@@ -33,17 +28,6 @@ class Internal(Cog):
 
     async def on_socket_raw_receive(self, _):
         self.socket_events += 1
-
-    @commands.command()
-    async def paginate(self, ctx, split_by: int, *, text):
-        """ Tests the paginator. """
-        pr = utils.Paginator(text, split_by)
-        await pr.paginate(ctx)
-
-    @commands.command()
-    async def humantime(self, ctx, *, time: converters.HumanTime):
-        """ Humantime debug. """
-        await ctx.send(f'```py\n{repr(time)}\n```')
 
     @commands.command()
     async def dstats(self, ctx):
@@ -185,53 +169,6 @@ class Internal(Cog):
         async with ctx.acquire() as conn:
             await conn.execute('DELETE FROM globalbans WHERE user_id = $1', who.id)
         await ctx.ok()
-
-    @commands.command()
-    async def prefix_cache(self, ctx, guild_id: int = None):
-        """ Inspects prefix cache for a guild. """
-        guild_id = guild_id if guild_id else ctx.guild.id
-        await ctx.send(f'`{repr(ctx.bot.prefix_cache.get(guild_id))}`')
-
-    @commands.command()
-    async def dping(self, ctx):
-        """ Detailed ping. """
-        def ms(before, after):
-            return str(round((after - before) * 1000, 2)) + 'ms'
-
-        # do it
-        before_send = monotonic()
-        msg = await ctx.send('...')
-        after_send = monotonic()
-        await msg.edit(content='..')
-        after_edit = monotonic()
-        await msg.delete()
-        after_delete = monotonic()
-
-        await ctx.send(DETAILED_PING.format(
-            ms(before_send, after_send),
-            ms(after_send, after_edit),
-            ms(after_edit, after_delete)
-        ))
-
-    @commands.group()
-    async def mem(self, ctx):
-        """ Memory statistics. """
-        pass
-
-    @mem.command()
-    async def count(self, ctx, type_name):
-        """ Counts the amount of an object tracked by the GC. """
-        cnt = objgraph.count(type_name)
-        if not cnt:
-            return await ctx.send('Not found.')
-        await ctx.send('`{}`: {}'.format(type_name, utils.commas(cnt)))
-
-    @mem.command()
-    async def common_types(self, ctx, limit: int=5):
-        """ Shows common types tracked by the GC. """
-        with io.StringIO() as stdout:
-            objgraph.show_most_common_types(limit=limit, file=stdout)
-            await ctx.send('```\n{}\n```'.format(stdout.getvalue()))
 
 
 def setup(bot):
