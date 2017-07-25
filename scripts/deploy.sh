d5052162840dc2d4ee9a43b3a56f506a8f8ce330
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-07-25 11:24:08 -0700

    Improve deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,8 @@ scp config.prod.yml shiba:~/dogbot/config.yml
 scp .env shiba:~/dogbot/.env
 
 ssh shiba -t << ENDSSH
-cd ~/dogbot; git pull
-cd ~/dogbot; docker-compose kill
+cd ~/dogbot; git fetch --all
+cd ~/dogbot; git reset --hard origin/master
+cd ~/dogbot; docker-compose stop
 cd ~/dogbot; docker-compose up -d --build --force-recreate
 ENDSSH
