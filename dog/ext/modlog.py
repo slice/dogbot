0837bdeed2d98b60a0089b081f7c9b632ae352c9
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-08-19 12:13:30 -0700

    Make person descriptions in modlog include IDs

--- dog/ext/modlog.py
+++ dog/ext/modlog.py
@@ -23,6 +23,17 @@ async def is_publicly_visible(bot, channel: discord.TextChannel) -> bool:
     return everyone_overwrite is None or everyone_overwrite[1].read_messages is not False
 
 
+def describe(person, *, before='', created=False, joined=False):
+    message = f'{person} (`{person.id}`)'
+    if before:
+        message += ' ' + before
+    if created:
+        message += f', created {utils.ago(person.created_at)}'
+    if joined and isinstance(person, discord.Member):
+        message += f', joined {utils.ago(person.joined_at)}'
+    return message
+
+
 class Modlog(Cog):
     def __init__(self, bot):
         super().__init__(bot)
@@ -43,13 +54,13 @@ class Modlog(Cog):
 
         if before.channel is not None and after.channel is None:
             # left
-            await send(self.modlog_msg(f'{voice} {member} left {before.channel}'))
+            await send(self.modlog_msg(f'{voice} {describe(member)} left {before.channel}'))
         elif before.channel is None and after.channel is not None:
             # joined
-            await send(self.modlog_msg(f'{voice} {member} joined {after.channel}'))
+            await send(self.modlog_msg(f'{voice} {describe(member)} joined {after.channel}'))
         elif before.channel != after.channel:
             # moved
-            await send(self.modlog_msg(f'{voice} {member} moved from {before.channel} to {after.channel}'))
+            await send(self.modlog_msg(f'{voice} {describe(member)} moved from {before.channel} to {after.channel}'))
 
     async def on_message_edit(self, before: discord.Message, after: discord.Message):
         if before.author.bot or before.content == after.content:
@@ -61,15 +72,17 @@ class Modlog(Cog):
 
         m_before = utils.prevent_codeblock_breakout(utils.truncate(before.content, 850))
         m_after = utils.prevent_codeblock_breakout(utils.truncate(after.content, 850))
-        fmt = f'\N{MEMO} Message by {before.author} edited: ```{m_before}``` to ```{m_after}```'
+        fmt = f'\N{MEMO} Message by {describe(before.author)} edited: ```{m_before}``` to ```{m_after}```'
         await self.bot.send_modlog(before.guild, self.modlog_msg(fmt))
 
     async def on_member_update(self, before: discord.Member, after: discord.Member):
         if before.nick != after.nick:
-            msg = self.modlog_msg(f'\N{NAME BADGE} Nick for {before} updated: `{before.nick}` to `{after.nick}`')
+            msg = self.modlog_msg(
+                f'\N{NAME BADGE} Nick for {describe(before)} updated: `{before.nick}` to `{after.nick}`')
             await self.bot.send_modlog(before.guild, msg)
         elif before.name != after.name:
-            msg = self.modlog_msg(f'\N{NAME BADGE} Username for {before} updated: `{before.name}` to `{after.name}`')
+            msg = self.modlog_msg(
+                f'\N{NAME BADGE} Username for {describe(before)} updated: `{before.name}` to `{after.name}`')
             await self.bot.send_modlog(before.guild, msg)
 
     async def on_raw_bulk_message_delete(self, message_ids, channel_id):
@@ -94,33 +107,33 @@ class Modlog(Cog):
         if msg.id in self.bulk_deletes or msg.id in self.do_not_log_deletes:
             return
 
+        # if this channel isn't publicly visible or deletes shouldn't be tracked, bail
         if (not await is_publicly_visible(self.bot, msg.channel) or
                 await self.bot.config_is_set(msg.guild, 'modlog_notrack_deletes')):
             return
 
-        if msg.author.bot:
-            if not await self.bot.config_is_set(msg.guild, 'modlog_filter_allow_bot'):
-                return
+
+        # if the author was a bot and we aren't configured to allow bots, return
+        if msg.author.bot and not await self.bot.config_is_set(msg.guild, 'modlog_filter_allow_bot'):
+            return
 
         content = utils.prevent_codeblock_breakout(utils.truncate(msg.content, 1500))
-        fmt = f'\U0001f6ae Message by {msg.author} deleted: ```{content}``` ({len(msg.attachments)} attachments)'
+        fmt = (f'\U0001f6ae Message by {describe(msg.author)} deleted: ```{content}``` ({len(msg.attachments)} '
+               'attachments)')
         await self.bot.send_modlog(msg.guild, self.modlog_msg(fmt))
 
     async def on_member_join(self, member: discord.Member):
         new = '\N{SQUARED NEW} ' if (datetime.datetime.utcnow() - member.created_at).total_seconds() <= 604800 else ''
-        msg = self.modlog_msg(f'\N{INBOX TRAY} {new}{member} joined, created {utils.ago(member.created_at)}')
+        msg = self.modlog_msg(f'\N{INBOX TRAY} {new}{describe(member, created=True)}')
         await self.bot.send_modlog(member.guild, msg)
 
     def format_member_departure(self, member, *, verb='left', emoji='\N{OUTBOX TRAY}'):
-        created_ago = utils.ago(member.created_at)
-
         # if it's a user, return bare info
         if isinstance(member, discord.User):
-            return f'{emoji} {member} {verb}, created {created_ago}'
+            return f'{emoji} {describe(member, before=verb, created=True)}'
 
         bounce = '\U0001f3c0 ' if (datetime.datetime.utcnow() - member.joined_at).total_seconds() <= 1500 else ''
-        joined_ago = utils.ago(member.joined_at)
-        return self.modlog_msg(f'{emoji} {bounce}{member} {verb}, created {created_ago}, joined {joined_ago}')
+        return self.modlog_msg(f'{emoji} {bounce}{describe(member, before=verb, created=True, joined=True)}')
 
     async def get_responsible(self, guild, target, action):
         try:
@@ -136,7 +149,7 @@ class Modlog(Cog):
         return f' with reason `{entry.reason}`' if entry.reason else ' with no attached reason'
 
     async def on_member_unban(self, guild, user):
-        base_msg = f'\N{HAMMER} {user} (`{user.id}`) was unbanned'
+        base_msg = f'\N{HAMMER} {describe(user)} was unbanned'
         ml_msg = self.modlog_msg(base_msg + '.')
         msg = await self.bot.send_modlog(guild, ml_msg)
 
@@ -148,14 +161,15 @@ class Modlog(Cog):
         if not entry:
             return
 
-        ml_msg = self.modlog_msg(f'{base_msg} by {entry.user}{self.format_reason(entry)}.')
+        ml_msg = self.modlog_msg(f'{base_msg} by {describe(entry.user)}{self.format_reason(entry)}.')
         await msg.edit(content=ml_msg)
 
     async def on_member_ban(self, guild, user):
         # don't make on_member_remove process this user's departure
         self.ban_debounces.append(user.id)
 
-        msg = await self.bot.send_modlog(guild, self.format_member_departure(user, verb='banned', emoji='\N{HAMMER}'))
+        msg = await self.bot.send_modlog(guild,
+            self.modlog_msg(self.format_member_departure(user, verb='banned', emoji='\N{HAMMER}')))
 
         if not msg:
             return
@@ -165,9 +179,9 @@ class Modlog(Cog):
         if not entry:
             return
 
-        fmt = self.format_member_departure(user, verb=f'banned by {entry.user}{self.format_reason(entry)}',
+        fmt = self.format_member_departure(user, verb=f'banned by {describe(entry.user)}{self.format_reason(entry)}',
                                            emoji='\N{HAMMER}')
-        await msg.edit(content=fmt)
+        await msg.edit(content=self.modlog_msg(fmt))
 
     async def on_member_remove(self, member):
         # this is called also when someone gets banned, but we don't want duplicate messages, so bail if
@@ -188,7 +202,8 @@ class Modlog(Cog):
             return
 
         reason = self.format_reason(entry)
-        fmt = self.format_member_departure(member, verb=f'kicked by {entry.user}{reason}', emoji='\N{WOMANS BOOTS}')
+        fmt = self.format_member_departure(member, verb=f'kicked by {describe(entry.user)}{reason}',
+                                           emoji='\N{WOMANS BOOTS}')
         await ml_msg.edit(content=fmt)
 
     @commands.command(hidden=True)
