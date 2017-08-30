cf31db0821e470e7574befa3aee36d399167cf06
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-08-29 22:18:06 -0700

    Rename to gfx.py to graphics.py

--- dog/core/utils/__init__.py
+++ dog/core/utils/__init__.py
@@ -4,7 +4,7 @@ Various utilities for Dogbot.
 
 from .enum import *
 from .formatting import *
-from .gfx import *
+from .graphics import *
 from .net import *
 from .queue import *
 from .zalgo import *
