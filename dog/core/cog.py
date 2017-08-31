7da561d7bf3affecce8b10b50818591ccebe0ba2
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-08-30 20:13:11 -0700

    Add logger attribute in Cog baseclass
    I don't feel like refactoring all of my cog code to use this attribute at the moment, so I'll just leave this here for now.

--- dog/core/cog.py
+++ dog/core/cog.py
@@ -1,4 +1,8 @@
+import logging
+
+
 class Cog:
     """ The Cog baseclass that all cogs should inherit from. """
     def __init__(self, bot):
         self.bot = bot
+        self.logger = logging.getLogger('cog.' + type(self).__name__.lower())
