99066b1d78ff342e45cc720dd21ef3fca42ed620
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-10-09 13:53:06 -0700

    clean up monitoring setup

--- dog/ext/monitoring/__init__.py
+++ dog/ext/monitoring/__init__.py
@@ -6,16 +6,15 @@ from .sentry import SentryErrorReporting
 
 
 def setup(bot):
-    if 'discordpw_token' in bot.cfg['monitoring']:
-        bot.add_cog(Abalbots(bot))
+    cogs = {
+        'discordpw_token': Abalbots,
+        'bugsnag': BugsnagReporting,
+        'datadog': Datadog,
+        'raven_client_url': SentryErrorReporting
+    }
 
-    if 'bugsnag' in bot.cfg['monitoring']:
-        bot.add_cog(BugsnagReporting(bot))
-
-    if 'datadog' in bot.cfg['monitoring']:
-        bot.add_cog(Datadog(bot))
-
-    if 'raven_client_url' in bot.cfg['monitoring']:
-        bot.add_cog(SentryErrorReporting(bot))
+    for config_key, cog in cogs.items():
+        if config_key in bot.cfg.get('monitoring', {}):
+            bot.add_cog(cog(bot))
 
     bot.add_cog(MonitorChannel(bot))
