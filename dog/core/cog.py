4bf0f9e7b18b856581abc9a43bb3d4c382be646d
sliceofcode <cheesy.fried.bacon@gmail.com>
2017-10-22 20:33:46 -0700

    add some shortcuts to cog class

--- dog/core/cog.py
+++ dog/core/cog.py
@@ -1,8 +1,19 @@
 import logging
 
 
+def _mirror(name):
+    @property
+    def _mirror(self):
+        return getattr(self.bot, name)
+    return _mirror
+
+
 class Cog:
-    """ The Cog baseclass that all cogs should inherit from. """
+    """The Cog baseclass that all cogs should inherit from."""
     def __init__(self, bot):
         self.bot = bot
         self.logger = logging.getLogger('cog.' + type(self).__name__.lower())
+
+    pgpool = _mirror('pgpool')
+    redis = _mirror('redis')
+    session = _mirror('session')
