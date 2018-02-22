c766b70c18db8b161f632504fc70c69c5d4c367e
slice <ryaneft@gmail.com>
2018-02-21 22:04:00 -0800

    move blacklisting code to cog

--- dog/bot.py
+++ dog/bot.py
@@ -1,7 +1,6 @@
 import aiohttp
 import discord
-from discord.ext.commands import is_owner
-from lifesaver.bot import Bot, command, Context
+from lifesaver.bot import Bot
 from lifesaver.bot.storage import AsyncJSONStorage
 
 
@@ -10,28 +9,9 @@ class Dogbot(Bot):
         super().__init__(*args, **kwargs)
         self.session = aiohttp.ClientSession(loop=self.loop)
         self.load_all()
-        self.add_command(self.blacklist)
-        self.add_command(self.unblacklist)
-        self.blocked = AsyncJSONStorage('blacklisted_users.json', loop=self.loop)
+        self.blacklisted_storage = AsyncJSONStorage('blacklisted_users.json', loop=self.loop)
 
     async def on_message(self, message: discord.Message):
-        if str(message.author.id) in self.blocked.all():
+        if message.author.id in self.blacklisted_storage:
             return
         await super().on_message(message)
-
-    @command(hidden=True)
-    @is_owner()
-    async def blacklist(self, ctx: Context, user: discord.User, *, reason=None):
-        """Blacklist someone from using the bot."""
-        await self.blocked.put(user.id, reason)
-        await ctx.ok()
-
-    @command(hidden=True)
-    @is_owner()
-    async def unblacklist(self, ctx: Context, user: discord.User):
-        """Unblacklist someone from using the bot."""
-        try:
-            await self.blocked.delete(user.id)
-        except KeyError:
-            pass
-        await ctx.ok()
---
+++ dog/ext/administration.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext.commands import is_owner
+from lifesaver.bot import Cog, Context, group, command
+
+
+class Administration(Cog):
+    @group(hidden=True, invoke_without_command=True)
+    @is_owner()
+    async def blacklist(self, ctx: Context, user: discord.User, *, reason=None):
+        """Blacklist someone from using the bot."""
+        await self.bot.blacklisted_storage.put(user.id, reason)
+        await ctx.ok()
+
+    @blacklist.command()
+    @is_owner()
+    async def stats(self, ctx: Context):
+        """Views blacklist statistics."""
+        amount = len(self.bot.blacklisted_storage.all())
+        await ctx.send(f'User(s) blocked: **{amount}**')
+
+    @blacklist.command()
+    @is_owner()
+    async def reason(self, ctx: Context, user: discord.User):
+        """Views the reason for someone's blacklisting."""
+        if user.id not in self.bot.blacklisted_storage:
+            await ctx.send(f'{user} is not blacklisted.')
+            return
+
+        reason = self.bot.blacklisted_storage.get(user.id)
+        if not reason:
+            await ctx.send(f'{user} is blacklisted for no reason.')
+        else:
+            await ctx.send(f'{user} is blacklisted for reason: {reason}')
+
+    @command(hidden=True)
+    @is_owner()
+    async def unblacklist(self, ctx: Context, user: discord.User):
+        """Unblacklist someone from using the bot."""
+        try:
+            await self.bot.blacklisted_storage.delete(user.id)
+        except KeyError:
+            pass
+        await ctx.ok()
+
+
+def setup(bot):
+    bot.add_cog(Administration(bot))
