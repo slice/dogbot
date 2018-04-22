4d578993ee6d4c3e8c7979409892d7af98bd97d1
slice <ryaneft@gmail.com>
2018-04-21 19:55:39 -0700

    feat: use uvloop automatically

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 from lifesaver.logging import setup_logging
 from dog.bot import Dogbot
 
+try:
+    import uvloop
+    import asyncio
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    print('early: using uvloop!')
+except ModuleNotFoundError:
+    pass
+
 setup_logging()
 Dogbot.with_config().run()
