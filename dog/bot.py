857e336e3fcc60ccd1ea3f40ee73494b8061198d
slice <ryaneft@gmail.com>
2018-04-22 17:45:17 -0700

    add guild configuration manager

--- dog/bot.py
+++ dog/bot.py
@@ -3,9 +3,55 @@ import discord
 from lifesaver.bot import Bot
 from lifesaver.bot.storage import AsyncJSONStorage
 from dog.web.server import app as webapp
+from ruamel.yaml import YAML
 
 from quart.serving import Server
 from quart.logging import create_serving_logger
+
+
+class GuildConfigManager:
+    def __init__(self, bot):
+        self.bot = bot
+        self.yaml = YAML(typ='safe')
+        self.persistent = AsyncJSONStorage('guild_configs.json', loop=bot.loop)
+
+    def _id(self, obj):
+        if isinstance(obj, discord.Guild):
+            return str(obj.id)
+        return str(obj)
+
+    def can_edit(self, user: discord.User, guild) -> bool:
+        # owners can always see the guild's configuration
+        if guild.owner == user:
+            return True
+
+        config = self.get(guild)
+        # no config, so only the owner is let in
+        if config is None:
+            return False
+
+        editors = config.get('editors', [])
+        if isinstance(editors, list):
+            return str(user) in editors or user.id in editors
+        elif isinstance(editors, int):
+            return user.id == editors
+        else:
+            return False
+
+    async def write(self, guild, config: str):
+        await self.persistent.put(self._id(guild), config)
+
+    def get(self, guild):
+        config = self.persistent.get(self._id(guild))
+        if config is None:
+            return None
+        return self.yaml.load(config)
+
+    def __getitem__(self, guild):
+        config = self.get(self._id(guild))
+        if not config:
+            raise KeyError(self._id(guild))
+        return config
 
 
 class Dogbot(Bot):
@@ -14,6 +60,7 @@ class Dogbot(Bot):
         self.session = aiohttp.ClientSession(loop=self.loop)
         self.load_all()
         self.blacklisted_storage = AsyncJSONStorage('blacklisted_users.json', loop=self.loop)
+        self.guild_configs = GuildConfigManager(self)
 
         webapp.bot = self
         webapp.secret_key = self.cfg.web['secret_key']
