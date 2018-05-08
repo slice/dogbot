572b01ac144f41c88f6525648d1a98efb8871195
slice <ryaneft@gmail.com>
2018-05-07 21:05:04 -0700

    time: create Map abstraction for map drawing

--- dog/ext/time.py
+++ dog/ext/time.py
@@ -1,4 +1,5 @@
 import datetime
+import functools
 import logging
 from collections import defaultdict
 from io import BytesIO
@@ -58,6 +59,93 @@ def draw_rotated_text(image, angle, xy, text, fill, *args, **kwargs):
     return draw.textsize(text=text, font=kwargs.get('font'))
 
 
+class Map:
+    def __init__(self, *, session: aiohttp.ClientSession, twelve_hour: bool = False, loop):
+        self.font = ImageFont.truetype('assets/SourceSansPro-Bold.otf', size=35)
+        self.image = Image.open('assets/timezone_map.png').convert('RGBA')
+        self.session = session
+        self.twelve_hour = twelve_hour
+        self.loop = loop
+
+        # defaultdict to keep track of users and their timezones.
+        #
+        # keys are a tuple: (time as pretty string, hour offset)
+        # values are a discord.Member
+        #
+        # this is because users can have the same time offset, but have different
+        # actual times because timezones are inconsistent. bleh.
+        self.timezones = defaultdict(list)
+
+    @property
+    def format(self):
+        if self.twelve_hour:
+            return '%I:%M %p'
+        else:
+            return '%H:%M:'
+
+    def close(self):
+        self.image.close()
+
+    def add_member(self, member: discord.Member, timezone: str):
+        now = datetime.datetime.now(pytz.timezone(timezone))
+
+        # calculate the hour offset, not accounting for dst
+        offset = (now.utcoffset().total_seconds() - now.dst().total_seconds()) / 60 / 60
+        formatted = now.strftime(self.format)
+
+        self.timezones[(formatted, offset)].append(member)
+
+    async def draw_member(self, member: discord.Member, x, y):
+        def target():
+            avatar = Image.open(fp=BytesIO(avatar_bytes)).convert('RGBA')
+            self.image.paste(avatar, box=(x, y), mask=avatar)
+
+        avatar_url = member.avatar_url_as(static_format='png', size=32)
+        async with self.session.get(avatar_url) as resp:
+            avatar_bytes = await resp.read()
+            await self.loop.run_in_executor(None, target)
+
+    async def draw_rotated_text(self, *args, **kwargs):
+        func = functools.partial(draw_rotated_text, *args, **kwargs)
+        return await self.loop.run_in_executor(None, func)
+
+    async def render(self):
+        def save():
+            buffer = BytesIO()
+            self.image.save(buffer, format='png')
+            buffer.seek(0)  # go back to beginning
+            return buffer
+        buffer = await self.loop.run_in_executor(None, save)
+        return buffer
+
+    async def draw(self):
+        # keep track of how far up we are for each hour offset, so there are no
+        # overlaps. (this is for each "pretty group")
+        offset_positions = defaultdict(int)
+
+        for (formatted, offset), members in self.timezones.items():
+            # x position based on hour, for appropriate positioning on the map
+            x = int((offset + 11) * 38.5 + 21)
+
+            begin_y = offset_positions[str(offset)]
+            y = begin_y if begin_y != 0 else 480  # start at 480 pixels down if we don't have an offset already
+
+            for member in members:
+                await self.draw_member(member, x, y)
+
+                # move up the map (32 for avatar, 5 for some padding)
+                y -= 32 + 5
+
+            size = await self.draw_rotated_text(
+                self.image, 90, (x - 7, y + 30), formatted, fill=(0, 0, 0, 255),
+                font=self.font
+            )
+
+            # register our offset (size[0] is text width)
+            # because it's rotated by 90 degrees, we use width instead of height
+            offset_positions[str(offset)] = y - size[0] - 30
+
+
 class Time(Cog):
     def __init__(self, bot):
         super().__init__(bot)
@@ -95,74 +183,31 @@ class Time(Cog):
     @cooldown(1, 5, BucketType.guild)
     async def map(self, ctx: Context):
         """Views a timezone map."""
-        font = ImageFont.truetype('assets/SourceSansPro-Bold.otf', size=35)
-        map_image = Image.open('assets/timezone_map.png').convert('RGBA')
-        map_draw = ImageDraw.Draw(map_image)
-        session = aiohttp.ClientSession()
 
-        is_twelvehour = False
         try:
             invoker_timezone = self.timezones.get(ctx.author.id)
-            country = next(country for (country, timezones) in pytz.country_timezones.items() if invoker_timezone
-                           in timezones)
-            is_twelvehour = country in TWELVEHOUR_COUNTRIES
+            country = next(
+                country for (country, timezones) in pytz.country_timezones.items() if invoker_timezone in timezones
+            )
+            twelve_hour = country in TWELVEHOUR_COUNTRIES
         except StopIteration:
             pass
 
-        timezones = defaultdict(list)
+        map = Map(session=self.bot.session, twelve_hour=twelve_hour, loop=self.bot.loop)
 
         for member in ctx.guild.members:
-            timezone = self.timezones.get(member.id)
-            if not timezone:
+            tz = self.timezones.get(member.id)
+            if not tz:
                 continue
-            now = datetime.datetime.now(pytz.timezone(timezone))
-            offset = (now.utcoffset().total_seconds() - now.dst().total_seconds()) / 60 / 60
-            time_here = datetime.datetime.now(pytz.timezone(timezone))
-            formatted = time_here.strftime('%I:%M %p' if is_twelvehour else '%H:%M')
-            timezones[(formatted, offset)].append(member)
+            map.add_member(member, tz)
 
-        drawn_in_offset = defaultdict(int)
+        await map.draw()
+        buffer = await map.render()
 
-        for (formatted, offset), members in timezones.items():
-            x = int((offset + 11) * 38.5 + 21)
-            log.debug('Checking drawn_in_offset for %d. (result=%d)', offset, drawn_in_offset[str(offset)])
-            my_offset = drawn_in_offset[str(offset)]
-            y = my_offset if my_offset != 0 else 480
-            log.debug('Y coordinate is now %d.', y)
-            for member in members:
-                log.debug('Drawing %s (%d).', member, member.id)
-                avatar_url = member.avatar_url_as(static_format='png', size=32)
-                async with session.get(avatar_url) as avatar_response:
-                    avatar_bytes = await avatar_response.read()
-                    avatar = Image.open(fp=BytesIO(avatar_bytes)).convert('RGBA')
-                    # resized = avatar.resize((32, 32), resample=Image.LANCZOS)
-                    log.info('map_image: Adding %s (%d) at (%d, %d)', member, member.id, x, y)
-                    map_image.paste(avatar, box=(x, y), mask=avatar)
-                y -= 32 + 5
-                log.debug('After drawing %s, Y coordinate is now %d.', member, y)
-            log.debug('Okay, drew this timezone. (time=%s, offset=%d. members=%s)', formatted, offset, members)
-            text_yofs = 30
-            ts_big = draw_rotated_text(
-                map_image, 90, (x - 7, y + text_yofs), formatted, fill=(0, 0, 0, 255),
-                font=font
-            )
-            registered_offset = y - ts_big[0] - 30
-            log.debug('Registering Y offset for this time offset (offset=%d, time=%s, y=%d)', registered_offset,
-                      formatted, y)
-            drawn_in_offset[str(offset)] = registered_offset
-
-        def render():
-            buffer = BytesIO()
-            map_image.save(buffer, format='png')
-            buffer.seek(0)
-            return buffer
-
-        buffer = await ctx.bot.loop.run_in_executor(None, render)
         file = discord.File(fp=buffer, filename=f'map_{ctx.guild.id}.png')
         await ctx.send(file=file)
-        map_image.close()
-        del map_draw
-        session.close()
+
+        map.close()
 
     @time.command(name='reset')
     async def time_reset(self, ctx: Context):
