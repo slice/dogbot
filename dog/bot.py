2a5d6317579fb90f908e34f8adaf21f246c1b235
slice <ryaneft@gmail.com>
2018-07-11 22:01:21 -0700

    Allow per-guild cog disabling

--- dog/bot.py
+++ dog/bot.py
@@ -1,6 +1,7 @@
 import logging
 
 import aiohttp
+import asyncio
 import discord
 from lifesaver.bot import Bot
 from lifesaver.bot.storage import AsyncJSONStorage
@@ -24,6 +25,49 @@ class Dogbot(Bot):
         webapp.bot = self
         webapp.secret_key = self.config.web['secret_key']
         self.boot_server()
+
+    def dispatch(self, event_name, *args, **kwargs):
+        """Modified version of the vanilla dispatch to fit disabled_cogs."""
+        discord.Client.dispatch(self, event_name, *args, **kwargs)
+
+        ev = 'on_' + event_name
+        guild = None
+
+        # yup, this can't go wrong at all
+        # extract the guild from A.guild or A if it's already a guild, with A
+        # being the first arg
+        first_arg = args[0] if args else None
+        if hasattr(first_arg, 'guild') and isinstance(first_arg.guild, discord.Guild):
+            guild = first_arg.guild
+        elif isinstance(first_arg, discord.Guild):
+            guild = first_arg
+
+        for event in self.extra_events.get(ev, []):
+            # if we have a guild and the event (method) qualified name has a .
+            # (which means we are inside of a cog), split the qualified name
+            # to grab the cog name then check the configuration to avoid
+            # dispatching if required
+            ev_name = event.__qualname__
+            if guild and '.' in ev_name:
+                cog_name, method_name = ev_name.split('.')
+                if self.cog_is_disabled(guild, cog_name):
+                    # log.debug('Dropping dispatch of %s to %s in %d -- cog disabled.', ev, ev_name, guild.id)
+                    continue
+
+            coro = self._run_event(event, event_name, *args, **kwargs)
+            asyncio.ensure_future(coro, loop=self.loop)
+
+    def cog_is_disabled(self, guild: discord.Guild, cog_name: str) -> bool:
+        config = self.guild_configs.get(guild)
+        disabled_cogs = config.get('disabled_cogs', [])
+        return cog_name in disabled_cogs
+
+    async def can_run(self, ctx, **kwargs):
+        cog_name = type(ctx.command.instance).__name__
+        if ctx.guild and self.cog_is_disabled(ctx.guild, cog_name):
+            return False
+
+        return await super().can_run(ctx, **kwargs)
 
     async def close(self):
         log.info('bot is exiting')
--- web/src/schema.js
+++ web/src/schema.js
@@ -3,7 +3,7 @@ import joi from 'joi-browser'
 // configuration schema
 export const schema = joi.compile(
   joi.object({
-    editors: joi.array().items(joi.number().label('user id')),
+    editors: joi.array().items([joi.number().label('user id'), joi.string().label('discordtag')]),
     autoresponses: joi.object().keys().pattern(/.{4}/, joi.string()),
     gatekeeper: joi.object({
       enabled: joi.boolean(),
@@ -11,7 +11,8 @@ export const schema = joi.compile(
       bounce_message: joi.string().min(1),
       broadcast_channel: joi.number().label('broadcast channel id')
     }),
-    measure_gateway_lag: joi.boolean()
+    measure_gateway_lag: joi.boolean(),
+    disabled_cogs: joi.array().items(joi.string().label('cog name'))
   })
 )
 
