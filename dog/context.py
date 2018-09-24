ff6f0204655439e93bab69dc23a9d1d7d0262cb9
slice <ryaneft@gmail.com>
2018-09-23 23:13:33 -0700

    Add emoji shortcuts to Context

--- dog/context.py
+++ dog/context.py
@@ -7,3 +7,12 @@ class Context(bot.Context):
     @property
     def pool(self):
         return self.bot.pool
+
+    def tick(self, variant: bool = True) -> str:
+        if variant:
+            return self.emoji('green_tick')
+        else:
+            return self.emoji('red_tick')
+
+    def emoji(self, name: str) -> str:
+        return self.bot.config.emoji[name]
