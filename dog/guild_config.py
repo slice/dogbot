59c8ceef5543f409532ad62e35e38e7a3ddef702
slice <ryaneft@gmail.com>
2018-09-23 19:15:31 -0700

    Check for role IDs in editors field
    Allow moderators to specify role IDs in the "editors" field in YAML
    configs.

--- dog/guild_config.py
+++ dog/guild_config.py
@@ -49,7 +49,11 @@ class GuildConfigManager:
 
         editors = config.get('editors', [])
         if isinstance(editors, list):  # list of ids
-            return str(user) in editors or user.id in editors
+            # user#discrim, user id, role id
+            if (str(user) in editors
+                    or user.id in editors
+                    or any(role.id in editors for role in member.roles)):
+                return True
         elif isinstance(editors, int):  # a single id
             return user.id == editors
         else:
