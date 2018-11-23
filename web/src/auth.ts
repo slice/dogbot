874a6efe76a8210740a3f40d15fbb78d9176d78c
slice <ryaneft@gmail.com>
2018-11-23 01:07:15 -0800

    Preserve auth state in localStorage
    * Stores the authentication state in localStorage.
    * Made sure that the app only renders routes when we know if the user is
      authenticated or not, by using a <p>Loading...</p>.

--- web/src/auth.ts
+++ web/src/auth.ts
@@ -10,3 +10,17 @@ export const AuthContext = React.createContext({
   authenticated: false,
   user: null,
 })
+
+export const STORAGE_KEY = '_dogbot_authentication_state'
+
+export function store(state: AuthState) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(state))
+}
+
+export function load(): AuthState | null {
+  const serialized = localStorage.getItem(STORAGE_KEY)
+  if (serialized == null) {
+    return null
+  }
+  return JSON.parse(serialized)
+}
--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -8,38 +8,66 @@ import Guilds from '../views/Guilds'
 import Login from '../views/Login'
 import AuthRoute from './AuthRoute'
 import Landing from '../views/Landing'
-import { AuthContext, AuthState } from '../auth'
+import { AuthContext, AuthState, load, store } from '../auth'
 
 const log = logFactory('auth')
 
-export default class App extends Component<{}, { auth: AuthState }> {
-  state = { auth: { authenticated: false, user: null } }
+export default class App extends Component<{}, { auth: AuthState | null }> {
+  state = { auth: null }
 
   async componentDidMount() {
-    log('fetching authentication state')
-    const authState = await API.get<{ active: boolean; user: User }>(
+    const state = load()
+
+    if (state == null) {
+      // no state was preserved.
+      log('no auth state was preserved. fetching...')
+      const newState = await this.fetchAuthState()
+      log('going to preserve...')
+      store(newState)
+    } else {
+      // auth state was preserved, restore.
+      log('auth state was preserved, loading.')
+      this.setState({ auth: state })
+    }
+  }
+
+  async fetchAuthState() {
+    const response = await API.get<{ active: boolean; user: User }>(
       '/auth/user'
     )
 
-    log('authentication state:', authState)
-    const { active: authenticated, user } = authState
-    this.setState({ auth: { authenticated, user } })
+    log('/auth/user response:', response)
+
+    const authState: AuthState = {
+      authenticated: response.active,
+      user: response.user,
+    }
+
+    this.setState({ auth: authState })
+    return authState
   }
 
   render() {
+    let routes
+    if (this.state.auth !== null) {
+      routes = (
+        <AuthContext.Provider value={this.state.auth!}>
+          <Router>
+            <>
+              <Route path="/login" exact component={Login} />
+              <Route path="/" exact component={Landing} />
+              <AuthRoute path="/guilds" exact component={Guilds} />
+            </>
+          </Router>
+        </AuthContext.Provider>
+      )
+    } else {
+      routes = <p>Loading...</p>
+    }
+
     return (
       <div id="app-wrapper">
-        <div id="content">
-          <AuthContext.Provider value={this.state.auth}>
-            <Router>
-              <>
-                <Route path="/login" exact component={Login} />
-                <Route path="/" exact component={Landing} />
-                <AuthRoute path="/guilds" exact component={Guilds} />
-              </>
-            </Router>
-          </AuthContext.Provider>
-        </div>
+        <div id="content">{routes}</div>
       </div>
     )
   }
