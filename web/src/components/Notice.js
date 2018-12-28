fe9d129c1a8bc5fd4aa8a9cf838e72bda963ee73
slice <ryaneft@gmail.com>
2018-12-28 03:10:26 -0800

    Add default notice mood

--- web/src/components/Notice.js
+++ web/src/components/Notice.js
@@ -1,14 +1,15 @@
 import styled from 'styled-components'
 
 const MOODS = {
+  default: ['hsl(220, 100%, 85%)', 'hsl(220, 100%, 95%)'],
   danger: ['hsl(0, 100%, 85%)', 'hsl(0, 100%, 95%)'],
   success: ['hsl(120, 100%, 75%)', 'hsl(120, 100%, 95%)'],
 }
 
 const Notice = styled.div`
   padding: 0.5em 1em;
-  border: solid 1px ${({ mood }) => MOODS[mood][0]};
-  background: ${({ mood }) => MOODS[mood][1]};
+  border: solid 1px ${({ mood = 'default' }) => MOODS[mood][0]};
+  background: ${({ mood = 'default' }) => MOODS[mood][1]};
   border-radius: 0.15rem;
   margin: 1rem 0;
 `
