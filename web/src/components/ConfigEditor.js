922f03384944e0c3e6664ffae4ed476efbfccc67
slice <ryaneft@gmail.com>
2019-02-16 18:39:40 -0800

    Change config editor theme to dawn

--- web/src/components/ConfigEditor.js
+++ web/src/components/ConfigEditor.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import AceEditor from 'react-ace'
 import 'brace/mode/yaml'
-import 'brace/theme/github'
+import 'brace/theme/dawn'
 
 import './ConfigEditor.scss'
 
 export default function ConfigEditor(props) {
-  return (
-    <AceEditor mode="yaml" theme="github" name="config-editor" {...props} />
-  )
+  return <AceEditor mode="yaml" theme="dawn" name="config-editor" {...props} />
 }
