4006ff09e0dc04cb54635bf0bafdd64a4dd6b41b
slice <ryaneft@gmail.com>
2019-03-11 17:36:14 -0700

    Clean up OAuth handling code

--- dog/web/auth.py
+++ dog/web/auth.py
@@ -1,5 +1,6 @@
 import random
 from urllib.parse import quote_plus
+from typing import Tuple
 
 import aiohttp
 from quart import Blueprint, g, jsonify as json, redirect, request, session
@@ -8,30 +9,41 @@ auth = Blueprint('auth', __name__)
 API_BASE = 'https://discordapp.com/api/v6'
 
 
-def redirect_url():
+def redirect_url() -> Tuple[str, str]:
+    """Generate a redirect URL, returning the state and URL."""
     state = hex(random.getrandbits(256))[2:]
-    url = API_BASE + '/oauth2/authorize?client_id='
-    url += str(g.bot.config.oauth['client_id'])
+
+    client_id = g.bot.config.oauth['client_id']
     redirect_uri = g.bot.config.oauth['redirect_uri']
-    url += f'&redirect_uri={quote_plus(redirect_uri)}'
-    url += f'&response_type=code&scope=identify&state={state}'
+
+    url = (
+        f'https://discordapp.com/oauth2/authorize'
+        f'?client_id={client_id}'
+        f'&redirect_uri={quote_plus(redirect_uri)}'
+        '&response_type=code'
+        '&scope=identify'
+        f'&state={state}'
+    )
+
     return state, url
 
 
-async def get_user(bearer):
-    headers = {'Authorization': 'Bearer ' + bearer}
-    async with aiohttp.ClientSession(headers=headers, raise_for_status=True) as s:
-        return await (await s.get(API_BASE + '/users/@me')).json()
+async def fetch_user(bearer: str) -> dict:
+    """Fetch information about a user from their bearer token."""
+    headers = {'Authorization': f'Bearer {bearer}'}
+    async with aiohttp.ClientSession(headers=headers, raise_for_status=True) as sess:
+        resp = await sess.get(f'{API_BASE}/users/@me')
+        return await resp.json()
 
 
-async def get_access_token(code, *, refresh=False):
-    ENDPOINT = API_BASE + '/oauth2/token'
+async def fetch_access_token(code: str, *, refresh: bool = False) -> str:
+    ENDPOINT = f'{API_BASE}/oauth2/token'
 
     data = {
         'client_id': str(g.bot.config.oauth['client_id']),
         'client_secret': g.bot.config.oauth['client_secret'],
         'grant_type': 'authorization_code',
-        'redirect_uri': g.bot.config.oauth['redirect_uri']
+        'redirect_uri': g.bot.config.oauth['redirect_uri'],
     }
 
     if refresh:
@@ -39,25 +51,28 @@ async def get_access_token(code, *, refresh=False):
     else:
         data['code'] = code
 
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-    async with aiohttp.ClientSession(raise_for_status=True) as session:
-        response = await session.post(ENDPOINT, data=data, headers=headers)
-        return (await response.json())['access_token']
+    async with aiohttp.ClientSession(raise_for_status=True) as sess:
+        resp = await sess.post(ENDPOINT, data=data, headers=headers)
+        data = await resp.json()
+        return data['access_token']
 
 
 @auth.route('/redirect')
 async def auth_redirect():
     if session.get('oauth_state') != request.args.get('state'):
         return 'invalid state', 401
+
     if 'code' not in request.args:
         return 'no code', 400
-    access_token = await get_access_token(request.args['code'])
+
+    access_token = await fetch_access_token(request.args['code'])
     session['token'] = access_token
-    user = await get_user(access_token)
+
+    user = await fetch_user(access_token)
     session['user'] = user
+
     return redirect('/guilds')
 
 
@@ -79,6 +94,7 @@ def auth_login():
 @auth.route('/profile')
 async def auth_profile():
     active = 'token' in session
+
     if not active:
         return json(None)
 
