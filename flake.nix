5e0e069a1ea5be1b98c05030d03fe02a88ea3365
slice <tinyslices@gmail.com>
2023-03-02 01:18:17 -0800

    Add package for building web

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
   inputs = {
     lifesaver.url = "path:/Users/slice/src/prj/lifesaver";
-    flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
+    nixpkgs.follows = "lifesaver/nixpkgs";
   };
 
-  outputs = { lifesaver, flake-utils, ... }:
+  outputs = { lifesaver, flake-utils, nix-filter, nixpkgs, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       lifesaver.lib.${system}.mkFlake ({ python, pkgs, ... }: {
         name = "dogbot";
@@ -19,5 +20,29 @@
           timezonefinder
         ];
         pythonPackageOptions.format = "pyproject";
-      }));
+        hardConfig = {
+          bot_class = "dog.bot:Dogbot";
+          config_class = "dog.config:DogConfig";
+          extensions_path = "./dog/ext";
+        };
+      }) // {
+        packages.web = nixpkgs.legacyPackages.${system}.buildNpmPackage rec {
+          name = "dogbot-web";
+          version = "0.0.0";
+
+          npmDepsHash = "sha256-t903c1mov02Pec7p/IGsSlIOHlN36aaCBllGxcD/4ao=";
+
+          postInstall = ''
+            echo "replacing $out with create-react-app build artifacts"
+            mv -v $out/lib/node_modules/dogbot-web/build/* $out/
+            echo "removing $out/lib"
+            rm -r $out/lib
+          '';
+          src = nix-filter.lib {
+            root = ./web;
+            include =
+              [ "src" "public" ./web/package.json ./web/package-lock.json ];
+          };
+        };
+      });
 }
